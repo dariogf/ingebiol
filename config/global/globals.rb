6b9c5d1ccb8099e83c723b755d8c74344af8ab56
dariogf <dariogf@gmail.com>
2009-03-25 11:57:11 +0100

    comments

--- config/global/globals.rb
+++ config/global/globals.rb
@@ -1,12 +1,17 @@
-
+# Path where projects are going to be saved
 DATA_PATH = File.join(BASE_PATH,'../','guidata')
 
+# Folder containing commands configuration
 COMMAND_CONFIG = File.join(CONFIG_PATH,'commands','command_config')
+
 
 USER_SCRIPTS_PATH = File.join(CONFIG_PATH,'scripts')
 STAGES_PATTERN = File.join(COMMAND_CONFIG,'stage*.json')
 
+# Standard attributes are saved in a file with this name
 STANDARD_ATTR_JSON = 'std_attr.json'
+
+
 GET_JOBINFO_SCRIPT = 'get_job_info.rb'
 
 JOBLIST_TITLES_JSON = 'joblist_titles.json'
