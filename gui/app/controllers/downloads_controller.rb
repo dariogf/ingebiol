b039920f67bbac7401cafdabffa28e50fcb7cc9d
Diego Dario Guerrero Fernandez <dariogf@osiris.scbi.uma.es>
2009-04-24 14:38:43 +0200

    add images to results

--- gui/app/controllers/downloads_controller.rb
+++ gui/app/controllers/downloads_controller.rb
@@ -15,6 +15,7 @@ class DownloadsController < ApplicationController
     if File.exists?(file_to_send)
       # send_file('/Volumes/Documentos/Progs/ruby/gui/public/images/iconAMSin.png')
       send_file(file_to_send)
+      #,:filename => 'pepe'
     else
       # print "File doesn't exists"
       #       
@@ -27,6 +28,27 @@ class DownloadsController < ApplicationController
     
     
     # render :nothing => true
+    
+    
+  end
+  
+  #-----------------------------------------
+  # download a file
+  #-----------------------------------------
+  def download_img
+    @id = params[:id]
+              
+    # print "descargando @id:" + @id
+        
+    file_to_send = File.join(DATA_PATH, session[:current_command], session[:user_email], session[:current_job_id], @id)
+    
+    
+    if File.exists?(file_to_send)
+      #send_data(file_to_send,{:type=>'img/png'})
+      send_file file_to_send, :type => 'image/png', :disposition => 'inline'
+    else
+    end
+    
     
     
   end
