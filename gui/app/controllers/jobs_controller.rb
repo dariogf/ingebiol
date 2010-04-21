3bc97ac3c33d6e8ddffb287513f84723e629f903
Diego Dario Guerrero Fernandez <dariogf@osiris.servadm.scbi.uma.es>
2010-04-21 14:05:19 +0200

    passenger json issue 1

--- gui/app/controllers/jobs_controller.rb
+++ gui/app/controllers/jobs_controller.rb
@@ -29,11 +29,13 @@ class JobsController < ApplicationController
     # Populate joblist
     @joblist = Joblist.new(data_path,script_path,titles_path)
     
+     job_list_json=@joblist.to_json
+    
      respond_to do |format|
       format.html #{ render :template => 'index.rjs' }
       format.js
-      format.json  { render :json => @joblist.to_json }
-      #format.xml  { render :xml => @joblist }
+      format.json  { render :json =>job_list_json }
+      format.xml  { render :xml => @joblist }
      end
 
 	end
